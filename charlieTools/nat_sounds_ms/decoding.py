2278c9bd90debc23247af2652266c74fe165f80d
crheller <charlieheller95@gmail.com>
2020-04-11 16:46:14 -0700

    adding results object for compressing decoding results

--- charlieTools/nat_sounds_ms/decoding.py
+++ charlieTools/nat_sounds_ms/decoding.py
@@ -7,6 +7,8 @@ CRH 04/10/2020
 import numpy as np
 import pandas as pd
 import itertools
+import pickle
+import os
 
 import logging
 
@@ -19,7 +21,7 @@ class DecodingResults():
     results for a given recording site easier.
     """
 
-    def __init__(self, df):
+    def __init__(self, df=None):
         """
         Init with the results data frame returned at the end of the dprime analysis
         Dprime analysis is pairwise, so each analysis returns results for many pairs of 
@@ -27,34 +29,39 @@ class DecodingResults():
         doing PLS dim reduction, might have results in 2D space, 3D space etc.)
         """
 
-        # set attributes with list of unique stimulus combinations / categories
-        self.all_stimulus_pairs = df.combo.unique().tolist()
-        self.spont_stimulus_pairs = df[df['category']=='spont_spont'].combos.unique().tolist()
-        self.spont_evoked_stimulus_pairs = df[df['category']=='spont_evoked'].combos.unique().tolist()
-        self.evoked_stimulus_pairs = df[df['category']=='evoked_evoked'].combos.unique().tolist()
+        if df is not None:
+            # set attributes with list of unique stimulus combinations / categories
+            self.all_stimulus_pairs = df.combo.unique().tolist()
+            self.spont_stimulus_pairs = df[df['category']=='spont_spont'].combo.unique().tolist()
+            self.spont_evoked_stimulus_pairs = df[df['category']=='spont_evoked'].combo.unique().tolist()
+            self.evoked_stimulus_pairs = df[df['category']=='evoked_evoked'].combo.unique().tolist()
 
-        # set attribute for number of unique dimensionality reductions
-        self.unique_subspace_dims = df.n_components.unique().tolist()
+            # set attribute for number of unique dimensionality reductions
+            self.unique_subspace_dims = df.n_components.unique().tolist()
 
-        # get df columns
-        self.numeric_keys = df.select_dtypes(include=['float64']).columns
-        self.object_keys = df.select_dtypes(include=['object']).columns
+            # get df columns
+            self.numeric_keys = df.select_dtypes(include=['float64']).columns
+            self.object_keys = df.select_dtypes(include=['object']).columns
 
-        # get number of jacknnifes
-        self.n_jacks = df.jack_idx.max()
+            # get number of jacknnifes
+            self.n_jacks = df.jack_idx.values.astype(np.float).max() + 1
 
-        
-        # Collapse over jackknifes. Save mean / sd of all numeric params.
-        log.info("Collapsing results over jackknifes")
-        # returns a dataframe for numeric results
-        self.numeric_results = self.collapse_numeric(df) 
-        
-        # objects = arrays (e.g. arrays of eigenvectors, evals, decoding vectors).
-        # returns a dictionary of data frames
-        self.array_results = self.collapse_objects(df)
+            
+            # Collapse over jackknifes. Save mean / sd of all numeric params.
+            log.info("Collapsing results over jackknifes")
+            # returns a dataframe for numeric results
+            self.numeric_results = self._collapse_numeric(df) 
+            
+            # objects = arrays (e.g. arrays of eigenvectors, evals, decoding vectors).
+            # returns a dictionary of data frames
+            self.array_results = self._collapse_objects(df)
+
+            # modify the above results by removing collapsing across spont repeats
+            log.info("Consolidating results by combining all spont bins")
+            self._consolidate_spont_results()
 
 
-    def collapse_numeric(self, df):
+    def _collapse_numeric(self, df):
         """
         Collapse numeric results over jackknifes. Return a dataframe with
         mean / sd for each stimulus pair / subspace dimensionality
@@ -67,7 +74,7 @@ class DecodingResults():
         return pd.concat([mean, sem], axis=1)
 
 
-    def collapse_objects(self, df):
+    def _collapse_objects(self, df):
         categories = ['jack_idx', 'combo', 'n_components']
         obj_cols = self.object_keys
         unique_results = list(itertools.product(self.all_stimulus_pairs, self.unique_subspace_dims))
@@ -76,6 +83,7 @@ class DecodingResults():
         results = {}
         index = pd.MultiIndex.from_tuples(unique_results, names=['combo', 'n_components'])
         for obj in obj_cols:
+            log.info("collapsing results for object: {}".format(obj))
             _df = df[[obj]+categories]
             results[obj] = pd.DataFrame(index=index, columns=['mean', 'sem'])
             for idx in index:
@@ -87,23 +95,140 @@ class DecodingResults():
 
         return results
 
-    def get_result(name, stim_pair, n_components):
 
-        if name in self.num_cols:
+    def _consolidate_spont_results(self):
+        """
+        Idea is that there are many spont vs. spont comparisons, and for each sound
+        there are many spont comparisons. In reality, there should we just one
+        spont. vs. spont result and one spont vs. evoked result (per evoked sound)
+
+        Perform the consolidation by modifying the already created numeric results df and
+        object results dict / dfs. Propogate error using sem_new = np.sqrt(sem1**2 + sem2**2 + ...)
+        """
+
+        def error_prop(x, axis=0):
+            return np.sqrt(np.sum(x**2, axis=axis)) / x.shape[0]
+
+        # SPONT vs. SPONT
+
+        # 1) deal with numeric results for spont spont
+        df = self.numeric_results
+        mean_cols = [c for c in df.columns if '_sem' not in c]
+        err_cols = [c for c in df.columns if '_sem' in c]
+
+        spont_spont_mean = df.loc[pd.IndexSlice[self.spont_stimulus_pairs, :], :][mean_cols].groupby(by='n_components').mean()
+        spont_spont_sem = df.loc[pd.IndexSlice[self.spont_stimulus_pairs, :], :][err_cols].groupby(by='n_components').apply(error_prop)
+        spont_spont = pd.concat([spont_spont_mean, spont_spont_sem], axis=1)
+        new_idx = pd.MultiIndex.from_tuples([pd.Categorical(('spont_spont', n_components)) 
+                                for n_components in spont_spont.index], names=['combo', 'n_components'])
+        spont_spont.set_index(new_idx, inplace=True)
+
+        # drop individual spont_spont pairs from master df
+        df = df[~df.index.get_level_values('combo').isin(self.spont_stimulus_pairs)]
+
+        # add new spont results to df
+        df = spont_spont.append(df)
+        self.numeric_results = df
+
+
+        # 2) deal with array results for spont_spont
+        for obj in self.object_keys:
+            df = self.array_results[obj]
+            sp_df = df.loc[pd.IndexSlice[self.spont_stimulus_pairs, :], :]
+
+            m = [x.mean(axis=0) for x in [np.vstack([np.expand_dims(a, 0) for a in arr[1]['mean'].values]) for arr in sp_df.groupby('n_components')]]
+            sem = [error_prop(x, axis=0) for x in [np.vstack([np.expand_dims(a, 0) for a in arr[1]['sem'].values]) for arr in sp_df.groupby('n_components')]]
+            components = [arr[0] for arr in sp_df.groupby('n_components')]
+            new_idx = pd.MultiIndex.from_tuples([pd.Categorical(('spont_spont', n_components)) 
+                                for n_components in components], names=['combo', 'n_components'])
+            new_df = pd.DataFrame(index=new_idx, columns=['mean', 'sem'])
+            new_df['mean'] = m
+            new_df['sem'] = sem
+
+            df = df[~df.index.get_level_values('combo').isin(self.spont_stimulus_pairs)]
+            df = new_df.append(df)
+            self.array_results[obj] = df
+
+        self.spont_stimulus_pairs = ['spont_spont']
+
+
+        # SPONT vs. EVOKED
+        df = self.numeric_results
+        unique_evoked_bins = np.unique([[c.split('_')[0], c.split('_')[1]] for c in self.evoked_stimulus_pairs])
+
+        # 1) deal with numeric results
+        new_sp_ev_pairs = []
+        for stim in unique_evoked_bins:
+            # get all spont / evoked combos
+            sp_ev = np.unique([c for c in self.spont_evoked_stimulus_pairs if stim in c])
+            m = df.loc[pd.IndexSlice[sp_ev, :], :][mean_cols].groupby(by='n_components').mean()
+            sem = df.loc[pd.IndexSlice[sp_ev, :], :][err_cols].groupby(by='n_components').apply(error_prop)
+            sp_ev_df = pd.concat([m, sem], axis=1)
+            new_idx = pd.MultiIndex.from_tuples([pd.Categorical(('spont_{}'.format(stim), n_components)) 
+                                for n_components in sp_ev_df.index], names=['combo', 'n_components']) 
+            sp_ev_df.set_index(new_idx, inplace=True)
+            df = sp_ev_df.append(df)
+            new_sp_ev_pairs.append('spont_{}'.format(stim))
+
+        # remove inividual spont_evoked pairs 
+        df = df[~df.index.get_level_values('combo').isin(self.spont_evoked_stimulus_pairs)] 
+
+        # save updated dataframe for numeric results
+        self.numeric_results = df
+
+        # 2) deal with object results
+        for obj in self.object_keys:
+            df = self.array_results[obj]
+            for stim in unique_evoked_bins:
+                sp_ev = np.unique([c for c in self.spont_evoked_stimulus_pairs if stim in c])
+                sp_df = df.loc[pd.IndexSlice[sp_ev, :], :]
+                m = [x.mean(axis=0) for x in [np.vstack([np.expand_dims(a, 0) for a in arr[1]['mean'].values]) for arr in sp_df.groupby('n_components')]]
+                sem = [error_prop(x, axis=0) for x in [np.vstack([np.expand_dims(a, 0) for a in arr[1]['sem'].values]) for arr in sp_df.groupby('n_components')]]
+                components = [arr[0] for arr in sp_df.groupby('n_components')]
+                new_idx = pd.MultiIndex.from_tuples([pd.Categorical(('spont_spont', n_components)) 
+                                    for n_components in components], names=['combo', 'n_components'])
+                new_df = pd.DataFrame(index=new_idx, columns=['mean', 'sem'])
+                new_df['mean'] = m
+                new_df['sem'] = sem
+
+                df = df[~df.index.get_level_values('combo').isin(self.spont_stimulus_pairs)]
+                df = new_df.append(df)
+                self.array_results[obj] = df
+
+        # update self.spont_evoked_stimulus_pairs
+        self.spont_evoked_stimulus_pairs = new_sp_ev_pairs       
+
+        # no need to return anything... just update object attributes
+
+
+    def get_result(self, name, stim_pair, n_components):
+
+        if name in self.numeric_keys:
             return [self.numeric_results.loc[pd.IndexSlice[stim_pair, n_components]][name],
                     self.numeric_results.loc[pd.IndexSlice[stim_pair, n_components]][name+'_sem']]
         
-        elif name in self.obj_cols:
+        elif name in self.object_keys:
             return [self.array_results[name].loc[pd.IndexSlice[stim_pair, n_components]]['mean'], 
                     self.array_results[name].loc[pd.IndexSlice[stim_pair, n_components]]['sem']]
 
         else:
             raise ValueError("unknown result column") 
 
+    
+    def save_pickle(self, fn):
+        log.info("Saving pickle to {}".format(fn))
+        with open(fn, 'wb') as handle:
+            pickle.dump(self, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        log.info('Success!')
 
+    def load_results(self, fn):
+        if not os.path.isfile(fn):
+            raise FileNotFoundError
 
-
-
+        log.info("loading pickle from {}".format(fn))
+        with open(fn, 'rb') as handle:
+            data = pickle.load(handle)
+        return data
 
 
 # =================================== Analysis functions to compute dprime =================================================
