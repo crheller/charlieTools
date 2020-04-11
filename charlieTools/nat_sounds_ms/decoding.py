fd0b8b1f34820ea946569aadef8a8f3387d516a0
crheller <charlieheller95@gmail.com>
2020-04-10 18:18:11 -0700

    update to new dprime tools for NAT sounds

--- charlieTools/nat_sounds_ms/decoding.py
+++ charlieTools/nat_sounds_ms/decoding.py
@@ -1,4 +1,83 @@
 """
-decoding tools for natural sounds analysis
-"""
+decoding tools for natural sounds analysis. Most is based on 
+analysis shown in Rumyantsev et al., 2020, Nature
 
+CRH 04/10/2020
+"""
+import numpy as np
+
+def compute_dprime(A, B, diag=False):
+    """
+    Compute discriminability between matrix A and matrix B
+    where both are shape N neurons X N reps.
+
+    Return:
+        dprime
+        decoding axis (with norm 1)
+        evals: (of mean covariance matrix)
+        evecs: (of mean covariance matrix)
+        dU: <A> - <B>
+    """
+    if A.shape[0] > A.shape[1]:
+        raise ValueError("Number of dimensions greater than number of observations. Unstable")
+
+    if A.shape[0] != B.shape[0]:
+        raise ValueError("Number of dimensions do not match between conditions")
+
+    if diag:
+        dprime, wopt, evals, evecs, dU = _dprime_diag(A, B)
+
+    else:
+        dprime, wopt, evals, evecs, dU = _dprime(A, B)
+
+    return dprime, wopt, evals, evecs, dU 
+
+
+def _dprime(A, B):
+    """
+    See Rumyantsev et. al 2020, Nature for nice derivation
+    """
+    usig = 0.5 * (np.cov((A.T - A.mean(axis=-1)).T) + np.cov((B.T - B.mean(axis=-1)).T))
+    u_vec = (A.mean(axis=-1) - B.mean(axis=-1))[np.newaxis, :]
+
+    try:
+        wopt = np.matmul(np.linalg.inv(usig), u_vec.T)
+    except:
+        print('WARNING, Singular Covariance, dprime infinite, set to np.nan')
+        return np.nan, np.nan, np.nan, np.nan, np.nan
+
+    dp2 = np.matmul(u_vec, wopt)[0][0]
+    if dp2 < 0:
+        dp2 = -dp2
+
+    evals, evecs = np.linalg.eig(usig)
+
+    return np.sqrt(dp2), wopt, evals, evecs, u_vec
+
+
+def _dprime_diag(A, B):
+    """
+    See Rumyantsev et. al 2020, Nature for nice derivation
+    """
+
+    # get numerator (optimal dprime)
+    dp, _, evals, evecs, _ = _dprime(A, B)
+    numerator = dp ** 2
+
+    usig = 0.5 * (np.cov((A.T - A.mean(axis=-1)).T) + np.cov((B.T - B.mean(axis=-1)).T))
+    u_vec = (A.mean(axis=-1) - B.mean(axis=-1))[np.newaxis, :]
+
+    # get denominator
+    usig_diag = np.zeros(usig.shape)
+    np.fill_diagonal(usig_diag, np.diagonal(usig))
+    denominator = u_vec @ np.linalg.inv(usig_diag) @ (usig @ np.linalg.inv(usig_diag)) @ u_vec.T
+    denominator = denominator[0][0]
+    if denominator < 0:
+        denominator = -denominator
+
+    dp2 = numerator / denominator
+
+    # best decoding axis ignoring correlations (reduces to direction of u_vec)
+    wopt_diag = np.linalg.inv(usig_diag) @ u_vec.T
+
+    return np.sqrt(dp2), wopt_diag, evals, evecs, u_vec
--- charlieTools/nat_sounds_ms/preprocessing.py
+++ charlieTools/nat_sounds_ms/preprocessing.py
@@ -1,4 +1,107 @@
 """
-Some very basic tools for matrix reshaping 
-and for preprocessing normalization steps.
+Some very basic tools for matrix reshaping, used to get data 
+in the correct shape for doing decoding analysis smoothly.
+
+(and for some preprocessing normalization / est/val splitting steps)
+
+CRH 04/10/2020
 """
+
+from itertools import combinations
+import numpy as np
+
+def dict_to_X(d):
+    """
+    Transform dictionary of spike counts (returned by nems.recording.extract_epochs)
+    into a matrix of shape: (Neuron X Repetition X Stimulus X Time)
+    """
+    
+    epochs = d.keys()
+    for i, epoch in enumerate(epochs):
+        r_epoch = d[epoch].transpose(1, 0, -1)[:, :, np.newaxis, :]
+        if i == 0:
+            X = r_epoch
+        else:
+            # stack on stimuli (epochs)
+            X = np.append(X, r_epoch, axis=2)
+    return X
+
+
+def flatten_X(X):
+    """
+    Transform X matrix (Neuron X Stimulus X Repetition X Time) into 
+    matrix of Neurons X Repetition * Stimulus * Time
+    """
+    if len(X.shape) != 4: raise ValueError("Input has unexpected shape")
+    return X.reshape(X.shape[0], -1)
+
+
+def fold_X(X_flat, nstim, nreps, nbins):
+    """
+    Invert the transformation done by cat_stimuli. 
+        X_flat is shape (Neurons X Stimulus * Repetition * Time)
+        and this fn returns matrix of shape (Neurons X Repetion X Stimulus X Time)
+    """
+    if len(X_flat.shape) != 2: raise ValueError("Input has unexpected shape")
+    return X_flat.reshape(X_flat.shape[0], nreps, nstim, nbins)
+
+
+def get_est_val_sets(X, njacks=10):
+    """
+    njacks 50-50 splits of the data matrix X.
+
+    X is shape Neurons X Reps X Stim.
+    """
+    nreps = X.shape[1]
+    n_test_reps = int(nreps / 2)
+    all_idx = np.arange(0, nreps)
+
+    # get list of all possible est / val rep idxs
+    test_idx = list(combinations(all_idx, n_test_reps))
+
+    # choose njacks random combos
+    test_idx = np.array(test_idx)[np.random.choice(range(0, len(test_idx)), njacks)]
+    train_idx = np.array([list(set(all_idx) - set(t)) for t in test_idx])
+
+    est = []
+    val = []
+
+    for jk in range(njacks):
+        est.append(X[:, train_idx[jk], :])
+        val.append(X[:, test_idx[jk], :])
+
+    return est, val
+
+
+def scale_est_val(est, val, mean=True, sd=True):
+    """
+    Scale est / val datasets and return normalized data.
+    If mean true, mean center data
+    If sd true, normalize by std
+
+    val gets normalized according to stats measured in est
+    """
+    est_new = est.copy()
+    val_new = val.copy()
+
+    if len(est) != len(val):
+        raise ValueError("len of est and val must be the same")
+
+    if (len(est[0].shape) != 3) | (len(val[0].shape) != 3):
+        raise ValueError("est / val elements must be shape neuron x rep x stim")
+
+    sets = len(est)
+
+    for i in range(sets):
+        u = est[i].mean(axis=(1, 2), keepdims=True)
+        std = est[i].std(axis=(1, 2), keepdims=True)
+
+        if mean:
+            est_new[i] = est_new[i] - u
+            val_new[i] = val_new[i] - u
+
+        if sd:
+            est_new[i] = est_new[i] / std
+            val_new[i] = val_new[i] / std
+
+    return est_new, val_new
