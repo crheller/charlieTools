a60c59619247d0d98f4bc0d393bd4d56a1468d78
crheller <charlieheller95@gmail.com>
2020-06-06 10:07:59 -0700

    only simulate test set. High reps

--- charlieTools/nat_sounds_ms/decoding.py
+++ charlieTools/nat_sounds_ms/decoding.py
@@ -390,21 +390,19 @@ def do_tdr_dprime_analysis(xtrain, xtest, nreps_train, nreps_test, tdr_data=None
         if sim1 | sim2 | sim12:
             # simulate data. If pupil mask is specified, use this to created simulated trials.
             if ptrain_mask is not None:
-                xtrain_sim = nat_preproc.fold_X(xtrain, nreps=nreps_train, nstim=2, nbins=1)
                 xtest_sim = nat_preproc.fold_X(xtest, nreps=nreps_test, nstim=2, nbins=1)
-                pmask_all = np.concatenate((ptrain_mask, ptest_mask), axis=1)[:, :, :, np.newaxis]
-                x_all = np.concatenate((xtrain_sim, xtest_sim), axis=1)
-                x_all, pup_mask_all = simulate_response(x_all, pmask_all, sim_first_order=sim1,
+                pmask_sim = ptest_mask[:, :, :, np.newaxis]
+                x_sim, pup_mask_sim = simulate_response(xtest_sim, pmask_sim, sim_first_order=sim1,
                                                                           sim_second_order=sim2,
                                                                           sim_all=sim12,
-                                                                          nreps=nreps_test,
+                                                                          nreps=5000,
                                                                           suppress_log=True)
                 # pull out simulated test set, that's balanced over pupil conditions
                 # do this by taking every other trial
                 # only simulate the "test" set. This way decoding axis always the same as raw data analysis
-                xtest = x_all[:, ::2, :, 0]
+                xtest = x_sim[:, ::2, :, 0]
                 xtest = xtest.reshape(xtest.shape[0], -1)
-                ptest_mask = pup_mask_all[:, ::2, :, 0]
+                ptest_mask = pup_mask_sim[:, ::2, :, 0]
                 
             else:
                 raise NotImplementedError("Can't do simulations without specifying a pupil mask. TODO: update decoding.simulate_response to handle this")
